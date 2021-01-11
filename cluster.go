1d0398c2376009279a7e06f2723048498cf0c6aa
Tomasz Mazur <T.Mazur@stud.elka.pw.edu.pl>
2021-01-11 22:20:57 +0100

    Add cluster configuration functions

--- cluster.go
+++ cluster.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 	"os/exec"
+	"time"
 )
 
 type Server struct {
 	ServerId string
+	NodeAddr string
 	ApiAddr  string
 }
 
@@ -23,16 +27,18 @@ func NewCluster(size int) Cluster {
 }
 
 func ManageCluster(c *Cluster, command []string) {
+	//TODO error handling
 	switch command[1] {
 	case "bootstrap":
-		BootstrapCluster(c, command)
+		BootstrapCluster(c, command, false)
 	case "add":
-		AddServer(c, command)
+		AddServer(c, command, false)
+	case "start":
+		StartCluster(c)
 	}
-
 }
 
-func BootstrapCluster(c *Cluster, command []string) error {
+func BootstrapCluster(c *Cluster, command []string, configStart bool) error {
 	if len(command) != 8 {
 		return errors.New("wrong command")
 	}
@@ -61,10 +67,13 @@ func BootstrapCluster(c *Cluster, command []string) error {
 		return errors.New("bootstrap error")
 	}
 
-	c.AddToConfig(Server{
-		ServerId: *nodeId,
-		ApiAddr:  *httpAddr,
-	})
+	if !configStart {
+		c.AddToConfig(Server{
+			ServerId: *nodeId,
+			NodeAddr: *nodeAddr,
+			ApiAddr:  *httpAddr,
+		})
+	}
 
 	if err := cmd.Wait(); err != nil {
 		return errors.New("bootstrap error")
@@ -73,7 +82,7 @@ func BootstrapCluster(c *Cluster, command []string) error {
 	return nil
 }
 
-func AddServer(c *Cluster, command []string) error {
+func AddServer(c *Cluster, command []string, configStart bool) error {
 	if len(command) != 10 {
 		return errors.New("wrong command")
 	}
@@ -104,13 +113,89 @@ func AddServer(c *Cluster, command []string) error {
 		return errors.New("bootstrap error")
 	}
 
-	c.AddToConfig(Server{
-		ServerId: *nodeId,
-		ApiAddr:  *httpAddr,
-	})
+	if !configStart {
+		c.AddToConfig(Server{
+			ServerId: *nodeId,
+			NodeAddr: *nodeAddr,
+			ApiAddr:  *httpAddr,
+		})
+	}
 
 	if err := cmd.Wait(); err != nil {
 		return errors.New("bootstrap error")
+	}
+
+	return nil
+}
+
+func LoadConfig(filename string) (*Cluster, error) {
+	var config Cluster
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	d := json.NewDecoder(f)
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func SaveConfig(c *Cluster, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	e := json.NewEncoder(f)
+	if err := e.Encode(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func StartCluster(c *Cluster) error {
+	//TODO error handling
+	if len(c.Nodes) < 1 {
+		return errors.New("cluster must contain at least one server")
+	}
+
+	l := c.Nodes[0]
+	cmd := []string{
+		"cluster",
+		"bootstrap",
+		"-nodeId",
+		l.ServerId,
+		"-nodeAddr",
+		l.NodeAddr,
+		"-httpAddr",
+		l.ApiAddr,
+	}
+
+	BootstrapCluster(c, cmd, true)
+	//TODO see if I can eliminate this sleep
+	time.Sleep(2 * time.Second)
+
+	for i := 1; i < len(c.Nodes); i++ {
+		f := c.Nodes[i]
+		cmd = []string{
+			"cluster",
+			"add",
+			"-nodeId",
+			f.ServerId,
+			"-nodeAddr",
+			f.NodeAddr,
+			"-httpAddr",
+			f.ApiAddr,
+			"-joinAddr",
+			l.ApiAddr,
+		}
+
+		AddServer(c, cmd, true)
 	}
 
 	return nil
