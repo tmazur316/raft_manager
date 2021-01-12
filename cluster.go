0dad02935ff5b7161631944224f49eb1dbc117af
Tomasz Mazur <T.Mazur@stud.elka.pw.edu.pl>
2021-01-12 13:56:26 +0100

    Add RemoveServer function

--- cluster.go
+++ cluster.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"time"
@@ -15,7 +17,8 @@ type Server struct {
 }
 
 type Cluster struct {
-	Nodes []Server
+	Nodes  []Server
+	Leader string
 }
 
 func (c *Cluster) AddToConfig(s Server) {
@@ -35,6 +38,8 @@ func ManageCluster(c *Cluster, command []string) {
 		AddServer(c, command, false)
 	case "start":
 		StartCluster(c)
+	case "remove":
+		RemoveServer(c, command)
 	}
 }
 
@@ -73,6 +78,7 @@ func BootstrapCluster(c *Cluster, command []string, configStart bool) error {
 			NodeAddr: *nodeAddr,
 			ApiAddr:  *httpAddr,
 		})
+		c.Leader = *httpAddr
 	}
 
 	if err := cmd.Wait(); err != nil {
@@ -106,6 +112,7 @@ func AddServer(c *Cluster, command []string, configStart bool) error {
 			return errors.New("wrong command")
 		}
 	}
+	//TODO change raft_tests path in scripts
 	cmd := exec.Command("scripts/join.sh", *nodeId, *nodeAddr, *httpAddr, *joinAddr)
 	err := cmd.Start()
 
@@ -128,30 +135,41 @@ func AddServer(c *Cluster, command []string, configStart bool) error {
 	return nil
 }
 
-func LoadConfig(filename string) (*Cluster, error) {
-	var config Cluster
-
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
+func RemoveServer(c *Cluster, command []string) error {
+	if len(command) != 6 {
+		return errors.New("wrong command")
 	}
 
-	d := json.NewDecoder(f)
-	if err := d.Decode(&config); err != nil {
-		return nil, err
+	var nodeId = new(string)
+	var httpAddr = new(string)
+
+	for i := 2; i < len(command); i += 2 {
+		switch command[i] {
+		case "-nodeId":
+			*nodeId = command[i+1]
+		case "-httpAddr":
+			*httpAddr = command[i+1]
+		default:
+			return errors.New("wrong command")
+		}
 	}
 
-	return &config, nil
-}
-
-func SaveConfig(c *Cluster, filename string) error {
-	f, err := os.Create(filename)
+	url := fmt.Sprintf("http://%s/remove/%s", c.Leader, *nodeId)
+	cl := &http.Client{}
+	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return err
 	}
 
-	e := json.NewEncoder(f)
-	if err := e.Encode(c); err != nil {
+	resp, err := cl.Do(req)
+	if err != nil {
+		return err
+	}
+
+	removeFromConfig(c, *nodeId)
+	//todo response and second request to delete data
+
+	if err := resp.Body.Close(); err != nil {
 		return err
 	}
 
@@ -198,5 +216,59 @@ func StartCluster(c *Cluster) error {
 		AddServer(c, cmd, true)
 	}
 
+	return nil
+}
+
+func ShutdownCluster() {
+
+}
+
+func LoadConfig(filename string) (*Cluster, error) {
+	var config Cluster
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	d := json.NewDecoder(f)
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func SaveConfig(c *Cluster, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	e := json.NewEncoder(f)
+	if err := e.Encode(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func removeFromConfig(c *Cluster, nodeID string) error {
+	var index int
+	var found bool
+
+	for i, v := range c.Nodes {
+		if v.ServerId == nodeID {
+			index = i
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return errors.New("unable to remove node from config")
+	}
+
+	c.Nodes = append(c.Nodes[:index], c.Nodes[index+1:]...)
 	return nil
 }
